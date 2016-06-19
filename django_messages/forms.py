1efd09f9ddb437d5d11e270714b88353d1062926
luis <luis.jav8@gmail.com>
2016-06-18 21:47:03 -0500

    remove recipient not needed

--- django_messages/forms.py
+++ django_messages/forms.py
@@ -15,7 +15,7 @@ class ComposeForm(forms.Form):
     """
     A simple default form for private messages.
     """
-    recipient = CommaSeparatedUserField(label=_(u"Recipient"))
+    #recipient = CommaSeparatedUserField(label=_(u"Recipient"))
     subject = forms.CharField(label=_(u"Subject"), max_length=140)
     body = forms.CharField(label=_(u"Body"),
         widget=forms.Textarea(attrs={'rows': '12', 'cols':'55'}))
